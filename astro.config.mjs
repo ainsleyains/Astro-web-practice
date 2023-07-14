04b8fed7a5ca5be8d82086588f1a9041f5c92451
Ainsley <aaa.ainsley@gmail.com>
2023-07-13 19:54:52 -0700

    create components

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,4 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [react()]
-});
+export default defineConfig();
